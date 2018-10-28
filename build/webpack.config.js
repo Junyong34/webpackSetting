225529440945fc0721f559e1a75ea4acc9193065
PARK-PC\Park <junni34@ex-em.com>
2018-10-28 20:11:35 +0900

    [webpack] 환경 주석 설명 추가

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,11 +4,15 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    // webpack4부터 mode가 존재 production development 두개로 환경 셋팅 가능
+    // webpack plugin megrge 사용하여 분기 처리 할 수 있다.
     mode: 'development',
-    devtool:'source-map',
+    devtool: 'source-map',
+    // build 대상 파일
     entry: {
         main: "./src/main.js"
     },
+    // output build 결과를 저장하는 경로
     output: {
         filename: "[name]-bundle.js",
         path: path.resolve(__dirname, "../dist"),
@@ -16,7 +20,7 @@ module.exports = {
     devServer: {
         // historyApiFallback: true,
         // noInfo: true,
-        contentBase: path.resolve(__dirname,"../dist"),
+        contentBase: path.resolve(__dirname, "../dist"),
         // open: true,
         // hot: true,
         // inline: true,
@@ -42,6 +46,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node-modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                loader: 'file-loader'
             }
         ]
     },
@@ -49,9 +57,17 @@ module.exports = {
         new CleanWebpackPlugin(["dist"], {
             root: path.resolve(__dirname, "..")
         }),
+        // 엔트리 포인트 중 하나의 모듈의 이름을 변경하거나 새로운 것을 추가하는 경우 생성된 번들은 빌드에서 이름이
+        // 변경되지만 index.html에 script는 여전히 이전의 이름을 참조한다.그렇게 때문에 우리는 주기적으로
+        // 번들링 된 파일을 index.html에 삽입을 시켜야하는 번거로움이 있다.
+        //     이런 불편함을 html - webpack - plugin이 해결해 준다.
+        //     html - webpack - plugin은 정의되어있는 기본 template을 기준으로 번들링 시에 새로운 html 파일을
+        // 생성해 준다.이때 파일을 생성할 때 번들링된 javascript 파일을 삽입해준다.
         new HtmlWebpackPlugin({
             title: "Jun Yong",
-            template: path.join(__dirname, "../example/index.html")
+            filename:'index.html', // 생성되는 파일이름
+            template: path.join(__dirname, "../example/index.html"),  //템플릿 파일 이름
+            chunks: ['css', 'index', 'app', 'system', 'monitor']
         })
     ],
     optimization: {
